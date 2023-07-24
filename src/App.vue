c262815af87396b18b8c3055b060a76a6bc68441
Tallyson Freitas <tallyson.eu@outlook.com>
2023-07-24 09:22:05 -0300

    fix erro mais uma vez

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
   import { reactive } from 'vue';
 
-  import Cabecalho from './components/cabecalho.vue';
-  import Formulario from './components/formulario.vue';
+  import Cabecalho from './components/Cabecalho.vue';
+  import Formulario from './components/Formulario.vue';
   import ListaDeTarefas from './components/ListaDeTarefas.vue';
   
   const estado =reactive({
